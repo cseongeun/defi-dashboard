66bf22c8a489c6e2f64ba591ffad32af190cef3a
cseongeun <cseongeun94@gmail.com>
2021-07-13 21:42:57 +0900

    asdf

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Header } from '../components/Header';
 import { Home } from '../pages';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
+    <Header>
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
+      </Header>
+
     </BrowserRouter>
   );
 };
